b80841c14b1fad4fd1acef9d042bb3f16164870f
muhammad2702 <92623180+muhammad2702@users.noreply.github.com>
2023-01-13 21:21:04 +0500

    Update download.py

--- download.py
+++ download.py
@@ -15,7 +15,7 @@ def download_model():
     )
 
     model = StableDiffusionPipeline.from_pretrained(
-        "Linaqruf/anything-v3.0", 
+        "Monan/fromkaggle", 
         scheduler=lms,
         use_auth_token=HF_AUTH_TOKEN
     )
